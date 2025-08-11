0a8b50c1a9635372c69ec5a2bddd0617e7daa4b2
Dana Davis <builtbydana@gmail.com>
2025-08-11 18:10:33 -0400

    feat(layout): update head section for improved SEO and remove unused favicon links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,90 +27,20 @@ export default async function RootLayout({
 
 	return (
 		<html lang="en">
-			<Head>
+			<head>
 				<title>Firehouze Supply Co</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta
 					name="description"
+					content="Firehouze Supply Co - Your one-stop shop for all things fire."
+				/>
+				<meta name="og:image" content="/public/og-image.png" />
+				<meta name="og:title" content="Firehouze Supply Co" />
+				<meta
+					name="og:description"
 					content="Your one-stop shop for all things fire."
 				/>
-				<link rel="icon" href="/public/favicon.ico" />
-				<link
-					rel="apple-touch-icon"
-					sizes="57x57"
-					href="/public/apple-icon-57x57.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="60x60"
-					href="/public/apple-icon-60x60.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="72x72"
-					href="/public/apple-icon-72x72.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="76x76"
-					href="/public/apple-icon-76x76.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="114x114"
-					href="/public/apple-icon-114x114.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="120x120"
-					href="/public/apple-icon-120x120.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="144x144"
-					href="/public/apple-icon-144x144.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="152x152"
-					href="/public/apple-icon-152x152.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/public/apple-icon-180x180.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="192x192"
-					href="/public/android-icon-192x192.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/public/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="96x96"
-					href="/public/favicon-96x96.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/public/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/public/manifest.json" />
-				<meta name="msapplication-TileColor" content="#ffffff" />
-				<meta
-					name="msapplication-TileImage"
-					content="/public/ms-icon-144x144.png"
-				/>
-				<meta name="theme-color" content="#ffffff" />
-			</Head>
+			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				<Header logo={logo} categories={categories} />
 				<main>{children}</main>
